220036952d1b1a40c220fa3c6df07054ce7f5beb
GarlandIan <ian.garland@mymail.champlain.edu>
2015-04-16 14:49:58 -0400

    Built class Character in character.h

--- AdvProgFinalProject/character.h
+++ AdvProgFinalProject/character.h
@@ -8,12 +8,32 @@ class Character
 		int armorClass;
 		int speed;
 		int hp;
+		int initiative;
 		int saves[3];
 		double skills[35];//make const
 		string name;
 
 	public:
-		//getters
-		//setters
+		Character();
+		Character(int abil[], int armorC, int s, int hp, int init, int saves[], double skills[], string name);
+		Character(const Character &c);
+		~Character();
 
+		void setAScores(int scores[]);
+		void setAClass(int ac);
+		void setSpeed(int s);
+		void setHP(int h);
+		void setInit(int i);
+		void setSaves(int sa[]);
+		void setSkills(double skills[]);
+		void setName(string n);
+
+		int getAScores();
+		int getAClass();
+		int getSpeed();
+		int getHP();
+		int getInit();
+		int getSaves();
+		double getSkills();
+		string getName();
 };
