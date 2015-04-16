0b5f95ab12f5384d4da9f27dfcb3efe534a514bf
Kody-G <kody.greeley@gmail.com>
2015-04-16 15:14:13 -0400

    Small Start at Menus
    Will be finished later.  Checking what each menu needs before I
    continue.

--- AdvProgFinalProject/functions.cpp
+++ AdvProgFinalProject/functions.cpp
@@ -1,1 +1,65 @@
 #include"npc.h"
+
+void startingMenu()
+{
+	int user_choice = -1;
+	do{
+
+		while (user_choice < 0 || user_choice > 2)
+		{
+			cout << "Please select whether you are a player or the Dungeon Master\n";
+			cout << "0 - Exit\n";
+			cout << "1 - Player Menu\n";
+			cout << "2 - Dungeon Master Menu\n";
+			cin >> user_choice;
+		}
+		if (user_choice == 0)
+		{
+			cout << "Goodbye!\n";
+			system("pause");
+			exit(1);
+		}
+		if (user_choice == 1)
+		{
+			outsideMenu();
+		}
+		if (user_choice == 2)
+		{
+			DMmenu();
+		}
+		if (user_choice)
+		{
+			cout << "Please select whether you are a player or the Dungeon Master\n";
+			cout << "0 - Exit\n";
+			cout << "1 - Player Menu\n";
+			cout << "2 - Dungeon Master Menu\n";
+			cin >> user_choice;
+		}
+	} while (user_choice);
+}
+
+void outsideMenu()
+{
+	int user_choice2 = -1;
+	while (user_choice2 < 0 || user_choice2 > 2)
+	{
+		cout << "Please select whether you are a player or the Dungeon Master\n";
+		cout << "0 - Back to Starting Menu\n";
+		cout << "1 - ...\n";
+		cout << "2 - ...\n";
+		cin >> user_choice2;
+	}
+	if (user_choice2 == 0)
+	{
+
+	}
+	if (user_choice2 == 1)
+	{
+		
+	}
+	if (user_choice2 == 2)
+	{
+		
+	}
+
+}
--- AdvProgFinalProject/main.cpp
+++ AdvProgFinalProject/main.cpp
@@ -2,6 +2,7 @@
 
 int main(){
 
+	startingMenu();
 	cout << "Work.";
 
 	cout << "Yes.";
