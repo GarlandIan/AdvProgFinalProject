e3b89c817043fd461101563200b2704da3f8779e
GarlandIan <ian.garland@mymail.champlain.edu>
2015-04-16 14:54:48 -0400

    Added to character.cpp
    files to be filled in with commands

--- AdvProgFinalProject/character.cpp
+++ AdvProgFinalProject/character.cpp
@@ -1,1 +1,91 @@
 #include"character.h"
+
+void setAScores(int scores[]){
+
+
+
+}
+
+void setAClass(int ac){
+
+
+
+}
+
+void setSpeed(int s){
+
+
+
+}
+
+void setHP(int h){
+
+
+
+}
+
+void setSaves(int sa[]){
+
+
+
+}
+
+void setSkills(double skills[]){
+
+
+
+}
+
+void setName(string n){
+
+
+
+}
+
+int getAScores(){
+
+
+
+}
+
+int getAClass(){
+
+
+
+}
+
+int getSpeed(){
+
+
+
+}
+
+int getHP(){
+
+
+
+}
+
+int getInit(){
+
+
+
+}
+
+int getSaves(){
+
+
+
+}
+
+double getSkills(){
+
+
+
+}
+
+string getName(){
+
+
+
+}
