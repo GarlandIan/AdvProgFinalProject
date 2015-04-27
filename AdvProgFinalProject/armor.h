b316a80f98bbc09cd2ab0a2a10757355e5093a7a
Kody-G <kody.greeley@gmail.com>
2015-04-26 23:23:33 -0400

    dEscriptions

--- AdvProgFinalProject/armor.h
+++ AdvProgFinalProject/armor.h
@@ -5,7 +5,14 @@ Assigment:     Final Project
 Date Assigned: 3/23/15
 Due Date:      4/26/15 at 11:59pm
 
-Discription:   The purpose of this lab is to 
+Description:   The purpose of this lab is to create base dice roll mechanics and loot generation for the Players (User) and Dungeon Master (Administrator)
+of the popular fantasy role playing game known as Pathfinder.  It is to be used in partnership with the rest of the normal version of the game as it does not
+retain every required action that players can possibly use in the game itself.  Using all the forumulas related to Player skills, there are various types of die 
+rolling.  Players receive different bonuses to their dice rolls from our already provided, generated character's skill statistics.  Players will be able to manage the
+character's health, initiative role (who goes first in combat), attack role (if they would hit or miss/ how much damage they would do), save checks (the ability to
+dodge something or take less damage), etc... As said earlier it is to be used to only perform these actions and the rest of the game still must be managed by the 
+Dungeon Master and Players regularly.  Players of this game understand the meaning of roles and Dungeon Masters are the ones who outside of this program determine
+how the rolls effect the Players.
 
 Certification of Authenticity:
    I certify that this is entirely my own work, except where I have given fully-
